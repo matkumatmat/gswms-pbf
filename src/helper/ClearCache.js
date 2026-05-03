2dd756d733d4b41c84e96ccc2ba38154c46f38bf
matkumatmat <mamattewahyu@gmail.com>
2026-05-04 01:29:36 +0700

    feat(product): update product page with bulk print functionality and enhance search logic

--- src/helper/ClearCache.js
+++ src/helper/ClearCache.js
@@ -2,3 +2,8 @@ function BUSTER_CACHE() {
   PropertiesService.getScriptProperties().deleteAllProperties();
   Logger.log("Cache has been reset");
 }
+
+function testLogin() {
+  const result = AuthService.login({ email: 'alphatest@gmail.com', password: 'alpha001?' });
+  console.log(result);
+}
